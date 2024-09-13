143aa32de6d809f22bc23f0bb7aab9c598c122cb
Igor_Sukhov <mebunu94@gmail.com>
2024-09-13 09:19:33 +0300

    index

--- blog/myblog/apps.py
+++ blog/myblog/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class MainConfig(AppConfig):
+class MyblogConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'main'
+    name = 'myblog'
